db1f9ef2e5245950ada2ac4a29e62069b2f3d428
xulihang <xulihanghai@163.com>
2024-09-18 17:59:29 +0800

    adjust timeout and blocksize

--- src/Templates.ts
+++ src/Templates.ts
@@ -14,8 +14,8 @@ export const readRate = `
     ],
     "BinarizationModes": [
       {
-        "BlockSizeX": 61,
-        "BlockSizeY": 61,
+        "BlockSizeX": 0,
+        "BlockSizeY": 0,
         "EnableFillBinaryVacancy": 1,
         "ImagePreprocessingModesIndex": -1,
         "LibraryFileName": "",
@@ -102,7 +102,7 @@ export const readRate = `
         "Mode": "TDM_GENERAL_WIDTH_CONCENTRATION"
       }
     ],
-    "Timeout": 3000
+    "Timeout": 2000
   },
   "Version": "3.0",
   "FormatSpecification": {
